4856d6d013ad2021b51e2b9efa63057c82a056cc
Odysseas Neslechanidis <odnes@tiptheiceberg.com>
2023-12-06 16:40:36 +0000

    Initial commit

---
+++ stefanostyros/stefanostyros/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ic#a4b*xyc9ie4f9r79zyb0b15dmqhu2gq^su9ti7x3dnyl-y_"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
